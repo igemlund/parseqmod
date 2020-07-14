fadb875a0db5d93f4290e0816d775754abea653e
Twicster <64321057+Twicster@users.noreply.github.com>
2020-07-14 22:58:24 +0200

    Add files via upload
    Additional files, here is the negative dataset, both raw and cleaned. From uniprot, reviewed, no AMP no antifungal. Duplicate entries and nonstandard aminoacids removed.

---
+++ DataCleaning/UniprotClean.py
@@ -0,0 +1,36 @@
+with open("UniprotNoAMPNoAFP.csv", "r") as f:
+    raw = f.read()
+dat = raw.split("\n")
+vec=[0]*(len(dat)-1)
+for i in range(len(dat)-1):
+    k=dat[i+1].split(";")
+    vec[i]=k[1]
+vec = list(set(vec))
+print(vec)
+
+#standard aminoacids
+standard = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
+
+f=open('basesuniprotnoampafp.txt','w')
+#writes all sequences consisting only of standard aminoacids to file.
+for i in vec:
+    if isinstance(i,str):
+        n=0
+        for p in i:
+            if p in standard:
+                continue
+            else:
+                n+=1
+        if n==0:
+            f.write(i+'\n')
+    else:
+        for k in i:
+            n = 0
+            for p in k:
+                if p in standard:
+                    continue
+                else:
+                    n += 1
+            if n == 0:
+                f.write(k + '\n')
+f.close()
